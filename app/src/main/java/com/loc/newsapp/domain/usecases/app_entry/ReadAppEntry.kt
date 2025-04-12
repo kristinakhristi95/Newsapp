b3580951dc6f275ac11f3acca92dda07b01971e4
kristinakhristi95 <krishchristian.9@gmail.com>
2025-04-12 09:55:50 -0400

    app entry

---
+++ app/src/main/java/com/loc/newsapp/domain/usecases/app_entry/ReadAppEntry.kt
@@ -0,0 +1,20 @@
+//Kristina Khristi 301483429
+//Rajan Boudel
+//Harikrisnan Parmeswaran
+//Maziar Hassanzadeh Moghaddam
+
+package com.loc.newsapp.domain.usecases.app_entry
+
+import com.loc.newsapp.domain.manager.LocalUserManager
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ReadAppEntry @Inject constructor(
+    private val localUserManger: LocalUserManager
+) {
+
+    operator fun invoke(): Flow<Boolean> {
+        return localUserManger.readAppEntry()
+    }
+
+}
