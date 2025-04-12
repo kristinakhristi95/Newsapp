fea14fdbc29d152cab95ab5c22d73abcf4039fab
kristinakhristi95 <krishchristian.9@gmail.com>
2025-04-12 09:56:21 -0400

    news app features

---
+++ app/src/main/java/com/loc/newsapp/data/remote/NewsApi.kt
@@ -0,0 +1,29 @@
+//Kristina Khristi 301483429
+//Rajan Boudel
+//Harikrisnan Parmeswaran
+//Maziar Hassanzadeh Moghaddam
+
+package com.loc.newsapp.data.remote
+
+import com.loc.newsapp.data.remote.dto.NewsResponse
+import com.loc.newsapp.util.Constants.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("everything")
+    suspend fun getNews(
+        @Query("sources") sources: String,
+        @Query("page") page: Int,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): NewsResponse
+
+    @GET("everything")
+    suspend fun searchNews(
+        @Query("q") searchQuery: String,
+        @Query("sources") sources: String,
+        @Query("page") page: Int,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): NewsResponse
+}
